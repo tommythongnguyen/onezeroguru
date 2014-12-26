580b1fe1760857b279870ed0d52a9819d32704f3
Tommy Nguyen <tommy.nguyen@logicdrop.com>
2014-12-26 10:49:15 -0500

    updated

--- www/js/app.js
+++ www/js/app.js
@@ -7,31 +7,21 @@ angular.module('website', ['ngAnimate','ngTouch','ngRoute','ui.bootstrap'])
 				   otherwise({redirectTo:"/"})
 
 }])
-	.controller('MainController', function ($scope,$interval) {
-        console.log("hello");
-        $scope.slides = [
-            {image: 'assets/images/slideShow/img00.jpg', description: 'Image 00'},
-            {image: 'assets/images/slideShow/img01.jpg', description: 'Image 01'},
-            {image: 'assets/images/slideShow/img02.jpg', description: 'Image 02'},
-            {image: 'assets/images/slideShow/img03.jpg', description: 'Image 03'},
-            {image: 'assets/images/slideShow/img04.jpg', description: 'Image 04'}
-        ];
-    $scope.webImageUrlArray=["assets/images/imageWebReferences/personalWebThumb.jpg","assets/images/imageWebReferences/martialArtWebThumb.jpg"];
+	.controller('MainController', ["$scope","$interval","MainService",function ($scope,$interval,MainService) {
+    $scope.slides = MainService.getSlides();
 
-    $scope.personalWebArray=["assets/images/personalWeb/mainPics/homePage1.jpg","assets/images/personalWeb/mainPics/photoPage2.jpg","assets/images/personalWeb/mainPics/photoPageShot3.jpg","assets/images/personalWeb/mainPics/musicPage4.jpg",
-                             "assets/images/personalWeb/mainPics/musicPageShot5.jpg","assets/images/personalWeb/mainPics/collectionPage6.jpg","assets/images/personalWeb/mainPics/collectionPageShot7.jpg",
-                             "assets/images/personalWeb/mainPics/aboutPage8.jpg","assets/images/personalWeb/mainPics/contactPage9.jpg"];
-    $scope.webImageThumbsArray =["assets/images/personalWeb/thumbs/thumbHomePage1.jpg","assets/images/personalWeb/thumbs/thumbPhotoPage2.jpg","assets/images/personalWeb/thumbs/thumbPhotoPageShot3.jpg","assets/images/personalWeb/thumbs/thumbMusicPage4.jpg",
-                 "assets/images/personalWeb/thumbs/thumbMusicPageShot5.jpg","assets/images/personalWeb/thumbs/thumbCollectionPage6.jpg","assets/images/personalWeb/thumbs/thumbCollectionPageShot7.jpg",
-                 "assets/images/personalWeb/thumbs/thumbAboutPage8.jpg","assets/images/personalWeb/thumbs/thumbContactPage9.jpg"];
+    $scope.webImageUrlArray= MainService.getWebImageUrlArray();
 
-    $scope.martialArtWebArray=["assets/images/martialArtWeb/mainPics/Home1.jpg","assets/images/martialArtWeb/mainPics/Gallery2.jpg","assets/images/martialArtWeb/mainPics/Kids3.jpg",
-                              "assets/images/martialArtWeb/mainPics/Women4.jpg","assets/images/martialArtWeb/mainPics/Schedule5.jpg","assets/images/martialArtWeb/mainPics/Contactus6.jpg"];
-    $scope.martialArtWebThumbArray=["assets/images/martialArtWeb/thumbs/thumbHome1.jpg","assets/images/martialArtWeb/thumbs/thumbGallery2.jpg","assets/images/martialArtWeb/thumbs/thumbKids3.jpg",
-                                    "assets/images/martialArtWeb/thumbs/thumbWomen4.jpg","assets/images/martialArtWeb/thumbs/thumbSchedule5.jpg","assets/images/martialArtWeb/thumbs/thumbContactus6.jpg"];
+    $scope.personalWebArray= MainService.getPersonalWebArray();
 
-    //$scope.urlImageArrayLength = $scope.slides.length;
-    $scope.popupWebArray=[];//array that content all the image of the websites that display on Popup
+    $scope.webImageThumbsArray = MainService.getWebImageThumbsArray();
+
+    $scope.martialArtWebArray= MainService.getMartialArtWebArray();
+
+    $scope.martialArtWebThumbArray= MainService.getMartialArtWebThumbArray();
+   
+    $scope.popupWebArray= [];
+
     $scope.timer=3000;
     $scope.popupTimer=2500;
     $scope.stopTimer="";
@@ -112,16 +102,16 @@ angular.module('website', ['ngAnimate','ngTouch','ngRoute','ui.bootstrap'])
     if (imageUrl ==='assets/images/imageWebReferences/personalWebThumb.jpg')
     {
         $scope.popupWebArray=$scope.personalWebArray; //copy the personal website to the popupWebArray
-      $scope.thumbImageWebSite=$scope.webImageThumbsArray;
+        $scope.thumbImageWebSite=$scope.webImageThumbsArray;
         $scope.isDisplayPopupImage=true;
-      console.log("it is here");
+        
     }
     else if(imageUrl==='assets/images/imageWebReferences/martialArtWebThumb.jpg')
     {
-      $scope.popupWebArray=$scope.martialArtWebArray;
-       $scope.thumbImageWebSite=$scope.martialArtWebThumbArray;
-      $scope.isDisplayPopupImage=true;
-      console.log("OR it is here");
+        $scope.popupWebArray=$scope.martialArtWebArray;
+        $scope.thumbImageWebSite=$scope.martialArtWebThumbArray;
+        $scope.isDisplayPopupImage=true;
+     
     }
    } ;
    //===============nextPopuImg===================
@@ -207,8 +197,94 @@ angular.module('website', ['ngAnimate','ngTouch','ngRoute','ui.bootstrap'])
       console.log("imgIndex",imgIndex);
       $scope.popupImageCounter=imgIndex;
     }
+
+  }])
+  //================Service============================
+    .service("MainService",function(){
+        var slides = [
+                          {image: 'assets/images/slideShow/img00.jpg', description: 'Image 00'},
+                          {image: 'assets/images/slideShow/img01.jpg', description: 'Image 01'},
+                          {image: 'assets/images/slideShow/img02.jpg', description: 'Image 02'},
+                          {image: 'assets/images/slideShow/img03.jpg', description: 'Image 03'},
+                          {image: 'assets/images/slideShow/img04.jpg', description: 'Image 04'}
+                      ],
+        webImageUrlArray=[ "assets/images/imageWebReferences/personalWebThumb.jpg",
+                                  "assets/images/imageWebReferences/martialArtWebThumb.jpg"
+                          ],
+
+        personalWebArray=[ "assets/images/personalWeb/mainPics/homePage1.jpg",
+                                  "assets/images/personalWeb/mainPics/photoPage2.jpg",
+                                  "assets/images/personalWeb/mainPics/photoPageShot3.jpg",
+                                  "assets/images/personalWeb/mainPics/musicPage4.jpg",
+                                  "assets/images/personalWeb/mainPics/musicPageShot5.jpg",
+                                  "assets/images/personalWeb/mainPics/collectionPage6.jpg",
+                                  "assets/images/personalWeb/mainPics/collectionPageShot7.jpg",
+                                  "assets/images/personalWeb/mainPics/aboutPage8.jpg",
+                                  "assets/images/personalWeb/mainPics/contactPage9.jpg"
+                            ],
+
+        webImageThumbsArray =[ "assets/images/personalWeb/thumbs/thumbHomePage1.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbPhotoPage2.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbPhotoPageShot3.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbMusicPage4.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbMusicPageShot5.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbCollectionPage6.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbCollectionPageShot7.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbAboutPage8.jpg",
+                                      "assets/images/personalWeb/thumbs/thumbContactPage9.jpg"
+                              ],
+
+        martialArtWebArray=[ "assets/images/martialArtWeb/mainPics/Home1.jpg",
+                                    "assets/images/martialArtWeb/mainPics/Gallery2.jpg",
+                                    "assets/images/martialArtWeb/mainPics/Kids3.jpg",
+                                    "assets/images/martialArtWeb/mainPics/Women4.jpg",
+                                    "assets/images/martialArtWeb/mainPics/Schedule5.jpg",
+                                    "assets/images/martialArtWeb/mainPics/Contactus6.jpg"
+                            ],
+
+        martialArtWebThumbArray=[  "assets/images/martialArtWeb/thumbs/thumbHome1.jpg",
+                                          "assets/images/martialArtWeb/thumbs/thumbGallery2.jpg",
+                                          "assets/images/martialArtWeb/thumbs/thumbKids3.jpg",
+                                          "assets/images/martialArtWeb/thumbs/thumbWomen4.jpg",
+                                          "assets/images/martialArtWeb/thumbs/thumbSchedule5.jpg",
+                                          "assets/images/martialArtWeb/thumbs/thumbContactus6.jpg"
+                                  ];
+       
+
+        //-------get slides----------
+        this.getSlides=function(){
+          return slides;
+        };
+        
+        //---------get webImageUrlArray-------
+        this.getWebImageUrlArray =function(){
+          return webImageUrlArray;
+        };
+        
+        //---------getpersonalWebArray------
+        this.getPersonalWebArray = function(){
+          return personalWebArray;
+        };
+       
+        //-----------get webImageThumbsArray-----
+        this.getWebImageThumbsArray=function(){
+          return webImageThumbsArray;
+        };
+       
+        //----------get martialArtWebArray----
+        this.getMartialArtWebArray=function(){
+          return martialArtWebArray;
+        };
+       
+        //---------get martialArtWebThumbArray-----
+        this.getMartialArtWebThumbArray=function(){
+          return martialArtWebThumbArray;
+        };
+       
     })
-	
+
+
+  //=================animation======================
 	.animation('.slide-animation', function () {
         return {
             beforeAddClass: function (element, className, done) {
